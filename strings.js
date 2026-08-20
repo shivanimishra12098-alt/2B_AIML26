becc694ca6797e46f098955a8823d49036bcf961
shivani mishra <shivanimishra12098@gmail.com>
2026-08-20 12:09:46 +0530

    Add strings.js

---
+++ strings.js
@@ -0,0 +1,20 @@
+let name = "Shivi";
+let age = 19;
+console.log(`My name is ${name} and I am ${age} years old.`);
+let str = "hello";
+console.log(str.toUpperCase());
+
+let st = "CAT";
+console.log(st.toLowerCase());
+
+let s = "JavaScript";
+console.log(s.charAt(4));
+
+let S = "Apple,Banana,Mango";
+let fruits = S.split(",");
+console.log(fruits);
+
+let str = "I love JavaScript";
+console.log(str.includes("JavaScript"));
+console.log(str.includes("Python"));
+console.log(str.replace("JavaScript", "Python"));
