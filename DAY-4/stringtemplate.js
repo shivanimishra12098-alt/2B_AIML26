64c2b30e3f93f04d7d1cbdd23d5c95c374f2de2c
shivani mishra <shivanimishra12098@gmail.com>
2026-09-03 11:08:22 +0530

    string template

---
+++ DAY-4/stringtemplate.js
@@ -0,0 +1,11 @@
+let firstname = "Shivi";
+let lastname = "mishra";
+console.log(`My name is ${firstname} ${lastname}.`);
+console.log(`My name is ${firstname.toUpperCase()} ${lastname.toUpperCase()}.`);
+
+let Firstname = "Shivi";
+let Lastname = "Mishra";
+console.log(`My name is ${Firstname} ${Lastname}.`);
+function Fullname(firstname, lastname) {
+    return `${firstname} ${lastname}`;
+}
